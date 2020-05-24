4be3dd180d71e69a300b27d0d75ee00bd9c93d7c
Constantin Pape <c.pape@gmx.net>
2020-05-24 23:19:08 +0200

    Ignore the viewer in magicgui decorator

--- napari_covid_if_annotations/gui.py
+++ napari_covid_if_annotations/gui.py
@@ -1,13 +1,12 @@
 from magicgui import magicgui
-from napari import Viewer
 from ._key_bindings import update_layers, toggle_hide_annotated_segments
 
 
-@magicgui(call_button='update layers [u]')
-def update_layers_gui(viewer: Viewer):
+@magicgui(call_button='update layers [u]', ignore=['viewer'])
+def update_layers_gui(viewer):
     update_layers(viewer)
 
 
-@magicgui(call_button='hide annotated cells [h]')
-def toggle_hide_annotated_segments_gui(viewer: Viewer):
+@magicgui(call_button='hide annotated cells [h]', ignore=['viewer'])
+def toggle_hide_annotated_segments_gui(viewer):
     toggle_hide_annotated_segments(viewer)
