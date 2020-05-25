9b65e116a89f9e6547e6f853a08277fcf6eb02c3
k-dominik <k-dominik@users.noreply.github.com>
2020-05-25 12:24:16 +0200

    added missing __main__

---
+++ napari_covid_if_annotations/__main__.py
@@ -0,0 +1,17 @@
+import argparse
+
+from .launcher import launch_covid_if_annotation_tool
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str, default=None)
+    parser.add_argument('--saturation_factor', type=float, default=1)
+    parser.add_argument('--edge_width', type=int, default=2)
+
+    args = parser.parse_args()
+    launch_covid_if_annotation_tool(args.path, args.saturation_factor, args.edge_width)
+
+
+if __name__ == '__main__':
+    main()
